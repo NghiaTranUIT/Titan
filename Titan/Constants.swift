d2d494bdcc1ed7d055aa91726a502d754cdbb7c7
Nghia Tran <changeyourcolor@gmail.com>
2016-09-25 20:48:24 +0700

    Added libpg.framework - Inlcuded macOS(64-bit) and iOS architecture

---
+++ Titan/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Titan
+//
+//  Created by Nghia Tran Vinh on 9/25/16.
+//  Copyright © 2016 fe. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    // App
+    struct App {
+        static let Version = "1.0.0"
+    }
+}

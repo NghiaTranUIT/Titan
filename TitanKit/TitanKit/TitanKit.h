1a9bf88a677f66508bc2f096fcd4308629e52f27
Nghia Tran <changeyourcolor@gmail.com>
2016-09-25 15:46:25 +0700

    Added TitanKit and TitaniOS as dependency project in Titan Workspace

---
+++ TitanKit/TitanKit/TitanKit.h
@@ -0,0 +1,19 @@
+//
+//  TitanKit.h
+//  TitanKit
+//
+//  Created by Nghia Tran Vinh on 9/25/16.
+//  Copyright © 2016 fe. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TitanKit.
+FOUNDATION_EXPORT double TitanKitVersionNumber;
+
+//! Project version string for TitanKit.
+FOUNDATION_EXPORT const unsigned char TitanKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TitanKit/PublicHeader.h>
+
+

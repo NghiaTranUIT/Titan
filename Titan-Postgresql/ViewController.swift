5fce30d4f2b268c1c37dd104ca9e27b415952f99
Nghia Tran <changeyourcolor@gmail.com>
2016-09-25 12:12:11 +0700

    Initial Commit

---
+++ Titan-Postgresql/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Titan-Postgresql
+//
+//  Created by Nghia Tran Vinh on 9/25/16.
+//  Copyright © 2016 fe. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

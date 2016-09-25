fa79d1cf99ae5af56b7b75b208ef2a2929d16cdf
Nghia Tran <changeyourcolor@gmail.com>
2016-09-25 12:39:13 +0700

    Configure framework targets, macOS, iOS apps

---
+++ Titan-Postgresql-iOS-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Titan-Postgresql-iOS-app
+//
+//  Created by Nghia Tran Vinh on 9/25/16.
+//  Copyright © 2016 fe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

188184757b68ba9034328ee1c98647ab121149b7
Nghia Tran <changeyourcolor@gmail.com>
2016-09-25 15:19:09 +0700

    Init new workspace

---
+++ Titan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Titan
+//
+//  Created by Nghia Tran Vinh on 9/25/16.
+//  Copyright © 2016 fe. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+

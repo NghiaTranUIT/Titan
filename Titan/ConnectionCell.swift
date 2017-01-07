06b75aee804b16a1c1951b6d6da6e892effda1db
Nghia Tran <vinhnghiatran@gmail.com>
2017-01-07 11:50:42 +0700

    Refactor ConnectionCell Design + logic

--- Titan/ConnectionCell.swift
+++ Titan/ConnectionCell.swift
@@ -10,19 +10,27 @@ import Cocoa
 
 class ConnectionCell: NSCollectionViewItem {
 
-    //
-    // MARK: - Outlet
-    
     
     //
     // MARK: - Variable
+    fileprivate var databaseObj: DatabaseObj?
     
+    //
+    // MARK: - Outlet
     
+    // Database icon
+    @IBOutlet weak var databaseIconImageView: NSImageView!
+    
+    // Title
+    @IBOutlet weak var titleLbl: NSTextField!
+    
+
     //
     // MARK: - View Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Base
         self.initBaseAbility()
     }
     
@@ -43,13 +51,40 @@ class ConnectionCell: NSCollectionViewItem {
     //
     // MARK: - Public
     func configureCell(with databaseObj: DatabaseObj) {
+        self.databaseObj = databaseObj
+        self.setupData()
+    }
+}
+
+
+//
+// MARK: - Private
+extension ConnectionCell {
+    
+    // Setup
+    fileprivate func setupData() {
+        guard let databaseObj = self.databaseObj else {return}
         
-        self.textField!.stringValue = databaseObj.name
+        // Title
+        self.titleLbl.stringValue = databaseObj.name
+        
+        // Select state
+        self.setupSelectionState()
     }
     
-    
-    /// Database Btn Tapped
-    @IBAction func databaseBtnTapped(_ sender: NSButton) {
+    fileprivate func setupSelectionState() {
         
+        // Selected
+        if self.isSelected {
+            self.databaseIconImageView.image = NSImage(named: "icon_database_selected")
+            self.view.backgroundColor = NSColor(hexString: "#F4F5F9")
+            self.titleLbl.textColor = NSColor(hexString: "#606872")
+            return
+        }
+        
+        // Non-selected
+        self.databaseIconImageView.image = NSImage(named: "icon_database")
+        self.view.backgroundColor = NSColor.white
+        self.titleLbl.textColor = NSColor(hexString: "#8D9298")
     }
 }

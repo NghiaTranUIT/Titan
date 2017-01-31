2b41c8f1d954b35dbbe3f76f1aadc4a25b41810d
Nghia Tran <vinhnghiatran@gmail.com>
2017-02-01 00:00:01 +0700

    Supported BaseSplitViewController. Too tired for working. Need some sleep

---
+++ Titan/BaseSplitViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseSplitViewController.swift
+//  Titan
+//
+//  Created by Nghia Tran on 1/31/17.
+//  Copyright © 2017 fe. All rights reserved.
+//
+
+import Cocoa
+
+class BaseSplitViewController: NSSplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
